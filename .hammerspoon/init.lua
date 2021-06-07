82067f15aa086e6b1506a310e55d91854282886e
David Scotson <david.scotson@gmail.com>
2021-06-07 12:45:39 +0100

    keys and symbols

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,8 @@
+-- ~/.hammerspoon/init.lua
+
+-- Specify your combination (your hyperkey)
+local hyper = { "cmd", "alt", "ctrl", "shift" }-- We use 0 to reload the configuration
+hs.hotkey.bind(hyper, "0", function()
+   hs.reload()
+ end)-- Notify about the config reload
+hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
