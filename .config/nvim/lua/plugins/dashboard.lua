cd6d3b2a2fe696159094667d4bab259764642f2d
David Scotson <david.scotson@synergy-learning.com>
2026-03-19 10:25:50 +0000

    standardise nvim dashboard keys

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -3,18 +3,47 @@ return {
 
   {
     "folke/snacks.nvim",
-    opts = {
-      dashboard = {
-        preset = {
-          header = [[
+    opts = function(_, opts)
+      opts.dashboard = opts.dashboard or {}
+
+      -- Replace <leader> with spacebar icon in key display
+      opts.dashboard.formats = opts.dashboard.formats or {}
+      opts.dashboard.formats.key = function(item, ctx)
+        local key = item.key:gsub("<[Ll]eader>", "󱁐 ")
+        return { key, hl = "key" }
+      end
+
+      -- Remap single-key shortcuts to full LazyVim equivalents
+      local mappings = {
+        f = "<leader>ff",
+        n = "<leader>fn",
+        p = "<leader>fp",
+        g = "<leader>sg",
+        r = "<leader>fr",
+        c = "<leader>fc",
+        s = "<leader>qs",
+        l = "<leader>l",
+      }
+      local keys = (opts.dashboard.preset or {}).keys or {}
+      local result = {}
+      for _, item in ipairs(keys) do
+        if mappings[item.key] then
+          item.key = mappings[item.key]
+        end
+        table.insert(result, item)
+      end
+      opts.dashboard.preset = opts.dashboard.preset or {}
+      opts.dashboard.preset.keys = result
+
+      opts.dashboard.preset.header = [[
               __
 ___     ___    ___   __  __ /\_\    ___ ___
  / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\
  /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \
   \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\
-   \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
-        },
-      },
-    },
+   \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]]
+
+      return opts
+    end,
   },
 }
